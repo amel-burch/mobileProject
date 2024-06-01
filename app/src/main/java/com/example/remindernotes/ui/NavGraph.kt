4a2d43a3934a2dc850474f55a877d59706e40223
Avdo <avdo.vladavic@stu.ibu.edu.ba>
2024-06-01 15:16:51 +0200

    Profile Screen and Bottom nav

--- app/src/main/java/com/example/remindernotes/ui/NavGraph.kt
+++ app/src/main/java/com/example/remindernotes/ui/NavGraph.kt
@@ -6,22 +6,27 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import com.example.remindernotes.ui.screens.TaskDetailScreen
 import com.example.remindernotes.ui.screens.TaskListScreen
+import com.example.remindernotes.ui.screens.ProfileScreen
 import com.example.remindernotes.viewmodel.TaskViewModel
 
-
-sealed class Screen(val route: String){
+sealed class Screen(val route: String) {
     object TaskList : Screen("task_list")
     object TaskDetail : Screen("task_detail")
+    object Calendar : Screen("calendar")
+    object Profile : Screen("profile")
 }
 
 @Composable
-fun NavGraph(navController: NavHostController, taskViewModel: TaskViewModel){
-    NavHost(navController = navController, startDestination = Screen.TaskList.route){
-        composable(Screen.TaskList.route){
-            TaskListScreen(navController,taskViewModel)
+fun NavGraph(navController: NavHostController, taskViewModel: TaskViewModel) {
+    NavHost(navController = navController, startDestination = Screen.TaskList.route) {
+        composable(Screen.TaskList.route) {
+            TaskListScreen(navController, taskViewModel)
         }
-        composable(Screen.TaskDetail.route){
-            TaskDetailScreen(navController,taskViewModel)
+        composable(Screen.TaskDetail.route) {
+            TaskDetailScreen(navController, taskViewModel)
+        }
+        composable(Screen.Profile.route) {
+            ProfileScreen(navController)
         }
     }
 }
